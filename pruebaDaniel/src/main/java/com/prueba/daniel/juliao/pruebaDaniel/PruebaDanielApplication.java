8abd2ff364401e19a8439a311074aba3f4725b82
daniel juliao <zigmainflables@gmail.com>
2021-12-18 22:29:41 -0500

    creando Proyecto de preguntas

---
+++ pruebaDaniel/src/main/java/com/prueba/daniel/juliao/pruebaDaniel/PruebaDanielApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.daniel.juliao.pruebaDaniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDanielApplication.class, args);
+	}
+
+}
---
+++ pruebaDaniel/src/test/java/com/prueba/daniel/juliao/pruebaDaniel/PruebaDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.daniel.juliao.pruebaDaniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
